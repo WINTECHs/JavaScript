3dfd71e41be79acbf86680770751b9f25d551c88
WINTECH <145253159+WINTECHs@users.noreply.github.com>
2024-08-18 08:53:43 +0000

    Code Complete

--- GuessTheNumber.js
+++ GuessTheNumber.js
@@ -1,1 +1,13 @@
+// take input from user
+let g_n = prompt("Guess a number between 1 to 10 : ")
 
+// generate a random number between 1 to 10
+let r_n = math.random(1, 10)
+
+// compare both number and show result
+if (g_n == r_n) {
+console.log("You Win")
+}
+else{
+    console.log("You Lose!\nThe number was",r_n)
+}
